1d1760a05f7b9e4cd31f1ea76d00041b89c5b1fe
cogentx <cogentx@live.com>
2019-05-19 20:54:16 -0230

    ci000-basic setup

--- app/src/game.ts
+++ app/src/game.ts
@@ -1,7 +1,59 @@
-import Phaser from 'phaser';
-import {MSG} from './services/constants';
+import Phaser from "phaser";
 
-let game;
-game = new Phaser.Game();
+let game: Phaser.Game;
+let gameConfig: Phaser.Types.Core.GameConfig;
 
-console.log(MSG, game);
+function startGame() {
+  gameConfig = {
+    parent: "app-container",
+    width: 480,
+    height: 640,
+    backgroundColor: 0xff00ff,
+    scene: [bootGame, playGame]
+  };
+  game = new Phaser.Game(gameConfig);
+  window.focus();
+  resizeGame();
+  window.addEventListener("resize", resizeGame);
+}
+
+class bootGame extends Phaser.Scene {
+  constructor() {
+    super("BootGame");
+  }
+  create() {
+    console.log('game is booting...');
+    game.scene.start("PlayGame");
+  }
+}
+
+class playGame extends Phaser.Scene {
+  constructor() {
+    super("PlayGame");
+  }
+  create() {
+    console.log('here is my awesome game!');
+  }
+}
+
+startGame();
+
+///////////////////
+function resizeGame() {
+  const canvas = document.querySelector("canvas");
+  const windowWidth = window.innerWidth;
+  const windowHeight = window.innerHeight;
+  const windowRatio = windowWidth / windowHeight;
+  const gameRatio = game.config.width / game.config.height;
+  console.log(windowRatio, gameRatio);
+
+  if (canvas) {
+    if (windowRatio < gameRatio) {
+      canvas.style.width = windowWidth + "px";
+      canvas.style.height = windowWidth / gameRatio + "px";
+    } else {
+      canvas.style.width = (windowHeight * gameRatio) + "px";
+      canvas.style.height = windowHeight + "px";
+    }
+  }
+}
