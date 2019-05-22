fb9b104d48987a8f183e1c387555f78b20a1140d
cogentx <cogentx@live.com>
2019-05-21 21:49:46 -0230

    jgg001-basic layout

--- app/game.ts
+++ app/game.ts
@@ -3,27 +3,15 @@ import Phaser from "phaser";
 let game: Phaser.Game;
 let gameConfig: Phaser.Types.Core.GameConfig;
 
-function startGame() {
-  gameConfig = {
-    parent: "app-container",
-    width: 480,
-    height: 640,
-    backgroundColor: 0xff00ff,
-    scene: [bootGame, playGame]
-  };
-  game = new Phaser.Game(gameConfig);
-  window.focus();
-  resizeGame();
-  window.addEventListener("resize", resizeGame);
-}
-
 class bootGame extends Phaser.Scene {
   constructor() {
     super("BootGame");
   }
+  preload() {
+    this.load.image("emptytile", require('./emptytile.png'));
+  }
   create() {
-    console.log('game is booting...');
-    game.scene.start("PlayGame");
+    this.scene.start("PlayGame");
   }
 }
 
@@ -32,11 +20,32 @@ class playGame extends Phaser.Scene {
     super("PlayGame");
   }
   create() {
-    console.log('here is my awesome game!');
+    for (let row = 0; row < 4; row++) {
+      for (let col = 0; col < 4; col++) {
+        this.add.image(100 + col * 200, 100 + row * 200, 'emptytile');
+
+      }
+    }
+    // this.add.image(100, 100, "emptytile");
   }
 }
 
-startGame();
+function startGame() {
+  gameConfig = {
+    parent: "app-container",
+    width: 900,
+    height: 900,
+    // backgroundColor: 0xff00ff,
+    backgroundColor: 0xecf0f1,
+    scene: [bootGame, playGame]
+  };
+  game = new Phaser.Game(gameConfig);
+  window.focus();
+  resizeGame();
+  window.addEventListener("resize", resizeGame);
+}
+
+window.onload = () => startGame();
 
 ///////////////////
 function resizeGame() {
@@ -44,7 +53,7 @@ function resizeGame() {
   const windowWidth = window.innerWidth;
   const windowHeight = window.innerHeight;
   const windowRatio = windowWidth / windowHeight;
-  const gameRatio = game.config.width / game.config.height;
+  const gameRatio:any = (game.config.width / game.config.height) || 1;
   console.log(windowRatio, gameRatio);
 
   if (canvas) {
